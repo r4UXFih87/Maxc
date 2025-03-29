a9c163a38d55e5769d208c3efb221d37ec55fc9d
MG <marcosguilherme38473@gmail.com>
2025-03-29 19:08:48 -0300

    Atualizar o trader_au.py

--- trader_au.py
+++ trader_au.py
@@ -49,7 +49,7 @@ def executar_trading():
             saldo_usdc = Saldo("USDC")
             saldo_pepe = Saldo("PEPE")
             dados_candles = obter_dados()
-            print(saldo_pepe,saldo_usdc)
+            print(saldo_pepe,saldo_usd)
 
             medias = Media()
             valor_moeda = medias.Fechamentos(1, dados_candles)
