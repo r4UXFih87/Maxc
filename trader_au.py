1c5797df87da608ab84f1866be32894373e275bd
MG <marcosguilherme38473@gmail.com>
2025-04-01 10:04:39 -0300

    Atualizar o trader_au.py

--- trader_au.py
+++ trader_au.py
@@ -101,4 +101,4 @@ def home():
 threading.Thread(target=executar_trading, daemon=True).start()
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 5000)))
+    app.run(host="0.0.0.0", port=int(os.getenv("PORT", 10000)))
